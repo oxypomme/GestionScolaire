5aa68065f130cbf76061f7f8ac2d519f78090c35
OxyTom <oxytom8@gmail.com>
2020-05-19 13:53:08 +0200

    main menu added + icon

---
+++ prj_gestionsco/u_acceuil.pas
@@ -0,0 +1,42 @@
+unit u_acceuil;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, Menus;
+
+type
+
+  { Tf_accueil }
+
+  Tf_accueil = class(TForm)
+    item_inscrit: TMenuItem;
+    item_statistiques: TMenuItem;
+    item_accueil: TMenuItem;
+    item_quitter: TMenuItem;
+    item_filiere: TMenuItem;
+    item_archive: TMenuItem;
+    item_archive_n1: TMenuItem;
+    item_inscrit_liste: TMenuItem;
+    item_archive_n2: TMenuItem;
+    item_liste: TMenuItem;
+    mnu_main: TMainMenu;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  f_accueil: Tf_accueil;
+
+implementation
+
+{$R *.lfm}
+
+{ Tf_accueil }
+
+end.
+
