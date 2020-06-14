f53b1926d680cc162fc9d10e8bf48ab694400e05
AvaN0x <clement.ricatte57@gmail.com>
2020-06-14 09:27:05 +0200

    stupid commas,,,

--- sql/sp_etudiant_note.sql
+++ sql/sp_etudiant_note.sql
@@ -6,9 +6,9 @@ PROCEDURE `sp_etudiant_note`(IN e_id varchar(5), OUT e_nomtable varchar(20))
 BEGIN
  DROP TABLE IF EXISTS resultat;
  CREATE TABLE resultat AS
-    SELECT * 
+    SELECT lib_court, CONCAT("x ", REPLACE(coef,'.',',')), IF(note = -1 ,'Non noté', REPLACE(coef,'.',','))
     FROM vue_comprend_liste
     WHERE id_etud = e_id
     ORDER BY lib_court;
-SET e_nomtable := 'resultat';
+ SET e_nomtable := 'resultat';
 END
